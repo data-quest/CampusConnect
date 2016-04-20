58ef5b55542f92dddf994c5a4a2eddef0ff74804
Rasmus Fuhse <fuhse@data-quest.de>
2016-04-20 09:57:22 +0200

    improve debugging

--- lib/ECSAuthToken.class.php
+++ lib/ECSAuthToken.class.php
@@ -48,8 +48,6 @@ class ECSAuthToken {
         $url .= stripos($_SERVER['REQUEST_URI'], "?") === false
             ? $_SERVER['REQUEST_URI']
             : substr($_SERVER['REQUEST_URI'], 0, strpos($_SERVER['REQUEST_URI'], "?"));
-        var_dump($parameter);
-        die();
 
         $realm = self::getRealm(
             $url,
